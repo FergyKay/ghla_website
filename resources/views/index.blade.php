34242ac2dce9b0b48b9729fa95d48f4d72e6c0a4
Ferguson <efergusonarthur@gmail.com>
2019-01-25 13:12:58 +0000

    Used Eloquent for Db Querying

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -555,8 +555,7 @@
                     {{--</div>--}}
                     {{--</div>--}}
                     <div class="embed-responsive embed-responsive-16by9">
-                        <iframe width="853" height="480" src="https://www.youtube.com/embed/y575xULuvkQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
+                        <iframe width="853" height="480" src="https://www.youtube.com/embed/pQOtgwFrxxI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                    </div>
                 </div>
             </div>
         </div>
